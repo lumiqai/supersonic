e6ffc034a2039446e1ca080a7eb6142dc57cf4a9
Lumiq <shoaib.mzn@gmail.com>
2024-08-17 13:32:23 +0530

    Update zh-CN.ts

--- webapp/packages/supersonic-fe/src/locales/zh-CN.ts
+++ webapp/packages/supersonic-fe/src/locales/zh-CN.ts
@@ -7,13 +7,13 @@ import settings from './zh-CN/settings';
 import pages from './zh-CN/pages';
 
 export default {
-  'navBar.lang': '语言',
-  'layout.user.link.help': '帮助',
-  'layout.user.link.privacy': '隐私',
-  'layout.user.link.terms': '条款',
-  'app.preview.down.block': '下载此页面到本地项目',
-  'app.welcome.link.fetch-blocks': '获取全部区块',
-  'app.welcome.link.block-list': '基于 block 开发，快速构建标准页面',
+  'navBar.lang': 'Lang',
+  'layout.user.link.help': 'Help',
+  'layout.user.link.privacy': 'Privacy',
+  'layout.user.link.terms': 'Terms',
+  'app.preview.down.block': 'Preview',
+  'app.welcome.link.fetch-blocks': 'Welcome',
+  'app.welcome.link.block-list': 'Welcome Block List',
   ...pages,
   ...globalHeader,
   ...menu,
